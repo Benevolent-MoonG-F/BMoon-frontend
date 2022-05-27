23065ef80b39affe1fe3e9b322dd5ee50709ffea
Godswill Adigwe <gcadigwe@gmail.com>
2022-05-27 04:10:31 +0100

    more import issues

--- components/BMS/bms.js
+++ components/BMS/bms.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import {Button} from '../../components/button/index';
-import styles from './bms.module.css';
-import Image from 'next/image';
-import Logo from '../../public/images/bmslogo.png';
-const appLink = '/app/new-game';
-import {Element} from 'react-scroll';
+import React from "react";
+import { Button } from "../components/button/index";
+import styles from "./bms.module.css";
+import Image from "next/image";
+import Logo from "../../public/images/bmslogo.png";
+const appLink = "/app/new-game";
+import { Element } from "react-scroll";
 
-export function BMS(){
-    return(
-        <Element className={styles.bms}>
-                <div className="card text-center bg-transparent">
-            <div className="card-body">
-                <div className={`${styles.header}`}>
-                <Image alt="logo" src={Logo}/>
-                <h5 className="text-white">BMS</h5>
-                </div>
-                
-                <p className="card-text text-white">Benevolent Moon Squares(BMS) is a fun crypto prognostication game that takes predictive markets to a more exciting level. Call the date and time of your favorite token doing a Moon Shot, win the pot while also donating to charity.</p>
-                <Button href={appLink}
-                                        btnClassName={styles.btnWrapper}
-                                        linkClassName={styles.btnText}
-                                    >Play Game</Button>
-  </div>
-</div>
-        </Element>
-        
-    )
+export function BMS() {
+  return (
+    <Element className={styles.bms}>
+      <div className='card text-center bg-transparent'>
+        <div className='card-body'>
+          <div className={`${styles.header}`}>
+            <Image alt='logo' src={Logo} />
+            <h5 className='text-white'>BMS</h5>
+          </div>
+
+          <p className='card-text text-white'>
+            Benevolent Moon Squares(BMS) is a fun crypto prognostication game
+            that takes predictive markets to a more exciting level. Call the
+            date and time of your favorite token doing a Moon Shot, win the pot
+            while also donating to charity.
+          </p>
+          <Button
+            href={appLink}
+            btnClassName={styles.btnWrapper}
+            linkClassName={styles.btnText}
+          >
+            Play Game
+          </Button>
+        </div>
+      </div>
+    </Element>
+  );
 }
